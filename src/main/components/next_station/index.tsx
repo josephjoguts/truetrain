555ea0856b9dfc9218f812359e18d2db5ac8ae10
josephjoguts <josephjoguts@yandex.ru>
2020-10-28 02:02:56 +0300

    header and 2/3 of main

--- src/main/components/next_station/index.tsx
+++ src/main/components/next_station/index.tsx
@@ -4,6 +4,35 @@ import TrainCard from "./cards/train_card";
 import SpeedCard from "./cards/speed_card";
 
 class NextStation extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  isVisible(el: Element) {
+    if (el != null) {
+      return el.getBoundingClientRect().top + 150 <= window.innerHeight;
+    }
+  }
+  handleScroll() {
+    const text = document.querySelector(
+      ".nextStation_content h1"
+    ) as HTMLElement;
+    const wrapper = document.querySelector(".nextStation_content");
+    if (text != null) {
+      if (wrapper != null) {
+        if (this.isVisible(wrapper)) {
+          text.style.transform = "translate(0)";
+        }
+      }
+    }
+  }
+  componentDidMount() {
+    document.addEventListener("scroll", this.handleScroll);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("scroll", this.handleScroll);
+  }
+
   render() {
     const nextStationGirl = require("src/images/nextStationGirl.png");
     return (
